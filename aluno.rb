5e7eaa10f97767c6df99a2fe9814f68d8eeddb3b
FernandoGurtlerWJ <fernando.gurtler@walljobs.com.br>
2023-05-26 11:51:50 -0300

    final aluno

--- aluno.rb
+++ aluno.rb
@@ -1,19 +1,23 @@
 class Aluno
-  @nome
-  @idade
-
-  def mudar_nome(nome)
+  attr_reader :nome, :idade
+  
+  def initialize(nome, idade)
     @nome = nome
+    @idade = idade
   end
+  # def mudar_nome(nome)
+  #   @nome = nome
+  # end
 
-  def mostra_nome
-    @nome
-  end
+  # def mostra_nome
+  #   @nome
+  # end
 end
 
-a1 = Aluno.new
+a1 = Aluno.new("diego", 35)
 
-a1.mudar_nome("diego")
+# a1.mudar_nome("diego")
 # a1.idade = 35
 
-puts a1.mostra_nome
+puts a1.nome
+puts a1.idade
