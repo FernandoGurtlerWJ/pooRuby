d7bda577fb5db0639be78082b9d2a951c45649d5
FernandoGurtlerWJ <fernando.gurtler@walljobs.com.br>
2023-05-26 12:03:03 -0300

    heranca e polimorfismo exemplos

---
+++ animal.rb
@@ -0,0 +1,50 @@
+class Animal
+  attr_reader :nome
+
+  def initialize(nome)
+    @nome = nome
+  end
+
+  def comunicar
+    puts "aqui tá na classe mãe ou super classe"
+  end
+end
+
+class Cachorro < Animal
+  attr_reader :raca
+  
+  def initialize(nome, raca)
+    super(nome)
+    @raca = raca
+  end
+
+  def comunicar
+    puts "cachorro aqui"
+  end
+end
+
+class Gato < Cachorro
+  # attr_reader :raca
+
+  def initialize(nome, raca)
+    super(nome, raca)
+  end
+
+  
+end
+
+a1 = Animal.new("animal 1")
+c1 = Cachorro.new("cachorro 1", "chihuahua")
+g1 = Gato.new("gato 1", "siames")
+
+puts a1.nome
+a1.comunicar
+puts "----------"
+puts c1.nome
+puts c1.raca
+c1.comunicar
+puts "----------"
+puts g1.nome
+puts g1.raca
+g1.comunicar
+
