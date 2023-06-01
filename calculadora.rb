5ba7021f21c41c8ad37572cc0c40e2196a2aba5d
FernandoGurtlerWJ <fernando.gurtler@walljobs.com.br>
2023-06-01 15:00:43 -0300

    final aula 2

---
+++ calculadora.rb
@@ -0,0 +1,24 @@
+class Calculadora
+  
+  def somar(*args)  #sobrecarga, recebe vários argumentos de uma só vez
+    lista = []
+    lista.push(*args)
+    lista.inject(:+)
+  end
+end
+
+c1 = Calculadora.new
+resultado = c1.somar(1,2,3,4,5)
+puts resultado
+
+# class Aluno
+#   def notas(*args)
+#     notas = []
+#     notas.push(*args)
+#     notas
+#   end
+# end
+
+# a1 = Aluno.new
+# notas_a1 = a1.notas(10, 5.9, 9.8)
+# puts notas_a1
